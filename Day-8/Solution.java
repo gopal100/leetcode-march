86071852eb90a25dd6e7dbf602f128cc60533cb9
gopal <gopal.work10@gmail.com>
2022-03-09 00:36:39 +0530

    leetcode march-2022 day-8

---
+++ Day-8/Solution.java
@@ -0,0 +1,40 @@
+/*
+https://leetcode.com/problems/linked-list-cycle/
+
+Linked List Cycle
+*/
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = null;
+        ListNode fast = null;
+        
+        if(head == null)
+            return false;
+        
+        slow = head;
+        fast = head;
+        
+        while(slow !=null && fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            
+            if(slow == fast)
+                return true;
+            
+        }
+        
+        return false;
+    }
+}
